6c1cefc632a06794cbef7b09f5dd99b1ff93a693
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2022-03-21 18:21:19 +0000

    Created and routed a 404 page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SingleProjectsPageComponent } from './single-projects-page/single-projects-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
-    { path: "single-page", component: SingleProjectsPageComponent }
+    { path: "single-page", component: SingleProjectsPageComponent },
+    { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SingleProjectsPageComponent } from './single-projects-page/single-projects-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingleProjectsPageComponent
+    SingleProjectsPageComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
