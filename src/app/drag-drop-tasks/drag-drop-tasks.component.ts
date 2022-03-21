cc1c36c11634583d8e9771777b713a0607c00c1e
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2022-03-21 18:53:35 +0000

    formatted code

--- src/app/drag-drop-tasks/drag-drop-tasks.component.ts
+++ src/app/drag-drop-tasks/drag-drop-tasks.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import {CdkDragDrop, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 
 @Component({
-  selector: 'app-drag-drop-tasks',
-  templateUrl: './drag-drop-tasks.component.html',
-  styleUrls: ['./drag-drop-tasks.component.css']
+    selector: 'app-drag-drop-tasks',
+    templateUrl: './drag-drop-tasks.component.html',
+    styleUrls: ['./drag-drop-tasks.component.css']
 })
 export class DragDropTasksComponent {
 
@@ -13,16 +13,16 @@ export class DragDropTasksComponent {
     done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
     drop(event: CdkDragDrop<string[]>) {
-      if (event.previousContainer === event.container) {
+        if (event.previousContainer === event.container) {
         moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-      } else {
+    } else {
         transferArrayItem(
-          event.previousContainer.data,
-          event.container.data,
-          event.previousIndex,
-          event.currentIndex,
+            event.previousContainer.data,
+            event.container.data,
+            event.previousIndex,
+            event.currentIndex,
         );
-      }
+    }
     }
 
 }
