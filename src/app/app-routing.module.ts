2beffd86bff348e474d675c2d35a4052f3d2f125
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2022-03-21 17:23:12 +0000

    created a route for the single page component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SingleProjectsPageComponent } from './single-projects-page/single-projects-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: "single-page", component: SingleProjectsPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
