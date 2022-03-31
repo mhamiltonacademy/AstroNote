ae1c3ca6f72f1488d60394b5e05256ccf451e727
jwford <jonathanwford156@gmail.com>
2022-03-31 09:43:19 -0400

    projects page uses dummy data

--- src/app/project-card/project-card.component.ts
+++ src/app/project-card/project-card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Project } from '../projects-page/projects-page.component';
 
 @Component({
   selector: 'app-project-card',
   templateUrl: './project-card.component.html',
   styleUrls: ['./project-card.component.css']
 })
-export class ProjectCardComponent implements OnInit {
+export class ProjectCardComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ // @ts-ignore 2564
+  @Input() project: Project;
 
 }
--- src/app/projects-page/projects-page.component.ts
+++ src/app/projects-page/projects-page.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
   styleUrls: ['./projects-page.component.css']
 })
-export class ProjectsPageComponent implements OnInit {
+export class ProjectsPageComponent {
+  projects: Project[] = [
+    {name: 'AstroNote', tasks: 5, inProgress: 3, deadline: '3/30/22'},
+    {name: 'Frontend', tasks: 2, inProgress: 0, deadline: '5/23/22'},
+    {name: 'Database', tasks: 12, inProgress: 0, deadline: '7/14/22'},
+    {name: 'Frontend', tasks: 2, inProgress: 0, deadline: '5/23/22'},
+    {name: 'AstroNote', tasks: 5, inProgress: 3, deadline: '3/30/22'},
+    {name: 'Database', tasks: 12, inProgress: 0, deadline: '7/14/22'},
+    {name: 'AstroNote', tasks: 5, inProgress: 3, deadline: '3/30/22'},
+    {name: 'Database', tasks: 12, inProgress: 0, deadline: '7/14/22'},
+    {name: 'Frontend', tasks: 2, inProgress: 0, deadline: '5/23/22'},
+  ]
+}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export interface Project {
+  name: string;
+  tasks: number;
+  inProgress: number;
+  deadline: string;
 }
