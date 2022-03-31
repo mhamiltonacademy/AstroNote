873df47c28e82d82bd6a167bc84eedbda7dc3155
jazmatos <65051399+jazminmatos@users.noreply.github.com>
2022-03-31 03:25:32 +0000

    implemented deleteTask functionality

--- src/app/drag-drop-tasks/drag-drop-tasks.component.ts
+++ src/app/drag-drop-tasks/drag-drop-tasks.component.ts
@@ -10,21 +10,21 @@ export class DragDropTasksComponent {
 
     todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
 
-    progress = ['Nap', 'Nap again', 'Nap a 3rd time']
+    progress = ['Nap', 'Nap again', 'Nap a 3rd time'];
 
     done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
 
     drop(event: CdkDragDrop<string[]>) {
         if (event.previousContainer === event.container) {
         moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
-    } else {
-        transferArrayItem(
-            event.previousContainer.data,
-            event.container.data,
-            event.previousIndex,
-            event.currentIndex,
-        );
-    }
+        } else {
+            transferArrayItem(
+                event.previousContainer.data,
+                event.container.data,
+                event.previousIndex,
+                event.currentIndex,
+            );
+        }
     }
 
     addTask() {
@@ -35,8 +35,20 @@ export class DragDropTasksComponent {
         console.log("EDITING a task")
     }
 
-    deleteTask() {
-        console.log("DELETING a task")
+    deleteTask(task: string, taskList: string) {
+        switch (taskList) {
+            case 'todo':
+                console.log("SwitchCase:", 'To Do list')
+                this.todo = this.todo.filter(t => t !== task);
+                break;
+            case 'progress':
+                console.log("SwitchCase:", 'In Progress list')
+                this.progress = this.progress.filter(t => t !== task);
+                break;
+            case 'done':
+                console.log("SwitchCase:", 'Done list')
+                this.done = this.done.filter(t => t !== task);
+                break;
+        }
     }
-
 }
